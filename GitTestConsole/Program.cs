8dca444ba808c9f8bb4192079202493754d4425b
dotnetwev <dotnetwev@hotmail.com>
2016-08-26 12:43:54 +0200

    toutlemonde

--- GitTestConsole/Program.cs
+++ GitTestConsole/Program.cs
@@ -10,7 +10,7 @@ namespace GitTestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bonjour");
+            Console.WriteLine("Bonjour tout le monde");
         }
     }
 }
